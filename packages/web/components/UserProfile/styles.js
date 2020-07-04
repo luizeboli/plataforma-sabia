f2027afa86529b7b9cd2caa08c01147fca33b623
Luiz Claudio <luizcns18@gmail.com>
2020-07-04 18:02:20 -0300

    Improve user profile component

--- packages/web/components/UserProfile/styles.js
+++ packages/web/components/UserProfile/styles.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { Link } from '../Link';
 
 export const Container = styled.section`
 	padding-top: 3rem;
@@ -27,36 +26,16 @@ export const SectionTitle = styled.h3`
 	border-radius: ${({ theme }) => theme.metrics.doubleRadius}rem;
 `;
 
-export const SectionItem = styled(Link)`
+export const SectionItem = styled.button`
 	display: flex;
 	align-items: center;
 	font-size: 1.6rem;
 	margin-bottom: 2rem;
 	padding-left: 2rem;
 	color: ${({ theme }) => theme.colors.secondary};
-
-	:hover {
-		color: ${({ theme }) => theme.colors.darkGreen};
-	}
-
-	svg {
-		fill: ${({ theme }) => theme.colors.secondary};
-		stroke: ${({ theme }) => theme.colors.secondary};
-		width: 2rem;
-		height: 2rem;
-		margin-right: 1rem;
-	}
-`;
-
-export const LogoutButton = styled.button`
-	display: flex;
-	align-items: center;
-	font-size: 1.6rem;
 	background: none;
 	border: none;
-	padding-left: 2rem;
-	margin-bottom: 2rem;
-	color: ${({ theme }) => theme.colors.secondary};
+	width: 100%;
 
 	:hover {
 		color: ${({ theme }) => theme.colors.darkGreen};
